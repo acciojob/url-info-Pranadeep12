8ac5d7a9722dd0d0d6048a3f99cba01d4e94ddc9
Pranadeep12 <129034610+Pranadeep12@users.noreply.github.com>
2025-08-26 19:14:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
-let currentURL = window.location.href;
-let urlLength = currentURL.length;
+var currentURL = window.location.href;
+var urlLength = currentURL.length;
 alert(The length of the URL is: ${urlLength});
